190b3c1c97b3865d83504c979676cd17ac0b8fa5
= <=>
2024-12-26 21:21:00 +0700

    Connect databse

---
+++ NET_API/DbConnect/DbConnectApp.cs
@@ -0,0 +1,18 @@
+﻿namespace NET_API.DbConnect;
+using Microsoft.EntityFrameworkCore;
+using NET_API.Models.Domain;
+
+public class DbConnectApp : DbContext
+{
+        public DbConnectApp(DbContextOptions<DbConnectApp> options) : base(options) { }
+    
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+        public DbSet<Image> Images { get; set; }
+
+}
--- NET_API/obj/Debug/net8.0/NET_API.AssemblyInfo.cs
+++ NET_API/obj/Debug/net8.0/NET_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NET_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8493f646e6f289fadc8a0831b62b5d12edfd607a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NET_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NET_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
