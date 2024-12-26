8493f646e6f289fadc8a0831b62b5d12edfd607a
= <=>
2024-12-26 17:08:02 +0700

    commit -init

---
+++ NET_API/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NET_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult getAllStudent()
+        {
+            string[] studentsName = new string[] { "bao", "binh", "Chi", "Duong" };
+            return Ok(studentsName);
+        }
+    }
+}
